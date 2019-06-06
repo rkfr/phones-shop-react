6a3c49b040e786f5f948885b61204592066c2152
rkfr <e.xbyvnqn@gmail.com>
2019-06-06 17:26:03 +0200

    Updates

---
+++ precache-manifest.e4f3c7687d54ddd08a04939ff9a8bd86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0911cc5a25aafa3cef427b1faa7ddbfb",
+    "url": "/phones-shop-react/index.html"
+  },
+  {
+    "revision": "99e2136d08633c1b45ad",
+    "url": "/phones-shop-react/static/css/main.1fa3f329.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/phones-shop-react/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "99e2136d08633c1b45ad",
+    "url": "/phones-shop-react/static/js/main.7f0ad31b.chunk.js"
+  },
+  {
+    "revision": "1f4cd9513beab86025bb",
+    "url": "/phones-shop-react/static/js/runtime~main.34c05d10.js"
+  }
+]);

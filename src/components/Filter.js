bed85b54ca0375974de7fec259c65ef12ab67acd
rkfr <e.xbyvnqn@gmail.com>
2019-11-14 18:37:49 +0200

    added react-select

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,40 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Select from 'react-select';
 
 import { SORT_BY_ALPHA, SORT_BY_NEWEST } from '../constants';
 
-export default class Filter extends React.Component {
-    state = {
-      isOpen: false,
-    }
+const Filter = ({ setSortType, setSearchWord, sortBy }) => {
+  const options = [
+    { value: SORT_BY_ALPHA, label: 'Alphabetical' },
+    { value: SORT_BY_NEWEST, label: 'Newest' },
+  ];
 
+  return (
+    <form className="box">
+      <span>Sort by:</span>
+      <Select
+        options={options}
+        defaultValue={sortBy}
+        setValue={sortBy}
+        onChange={({ value }) => setSortType(value)}
+      />
 
-    render() {
-      const { setSortType, setSearchWord } = this.props;
-      const { isOpen } = this.state;
-      const selectClassName = `sorting-filter ${isOpen && 'sorting-filter--open'}`;
+      <label htmlFor="search">
+        <input
+          id="search"
+          className="search"
+          placeholder="Search..."
+          onChange={({ target }) => setSearchWord(target.value)}
+        />
+      </label>
+    </form>
+  );
+};
 
-      return (
-        <form>
-          <label className="box">
-                    Sort by:
-            <select
-              className={selectClassName}
-              onChange={({ target }) => {
-                setSortType(target.value);
-              }}
-            >
-              <option value={SORT_BY_ALPHA}>Alphabetical</option>
-              <option value={SORT_BY_NEWEST}>Newest</option>
-            </select>
-          </label>
-          <label>
-            <input
-              className="search"
-              placeholder="Search..."
-              onChange={({ target }) => setSearchWord(target.value)}
-            />
-          </label>
-        </form>
-      );
-    }
-}
+export default Filter;

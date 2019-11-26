e8cc08263228c9ef25888f00507a695f7bd70736
rkfr <e.xbyvnqn@gmail.com>
2019-11-26 18:37:26 +0200

    now if there are many pages with product cards, the page list is collapsed and all page numbers are not displayed

--- src/components/Catalog/Pagination/Pagination.js
+++ src/components/Catalog/Pagination/Pagination.js
@@ -45,6 +45,24 @@ const Pagination = ({
     updateCurrentPage(1);
   };
 
+  const restPages = (pages) => (
+    <>
+      {currentPage === pages && (
+        <>
+          <li className="pages-list__item">1</li>
+          <li className="pages-list__item">...</li>
+        </>
+      )}
+      <li className="pages-list__item current-item">{currentPage}</li>
+      {currentPage !== pages && (
+        <>
+          <li className="pages-list__item">...</li>
+          <li className="pages-list__item">{pages}</li>
+        </>
+      )}
+    </>
+  );
+
   const mapPages = (pages) => new Array(pages).fill(0).map((_, i) => {
     const page = i + 1;
 
@@ -64,7 +82,7 @@ const Pagination = ({
         <button type="button" aria-label="First page" onClick={onFirstPage} className="pagination__button pagination__button--first" />
         <button type="button" aria-label="Previous page" onClick={onPrevPage} className="pagination__button pagination__button--prev" />
         <ul className="pages-list">
-          {mapPages(allPages)}
+          {(allPages > 6) ? restPages(allPages) : mapPages(allPages)}
         </ul>
         <button type="button" aria-label="Next page" onClick={onNextPage} className="pagination__button pagination__button--next" />
         <button type="button" aria-label="Last page" onClick={onLastPage} className="pagination__button pagination__button--last" />

7181c089f27022a785c68c5885fb646ac0df811f
rkfr <e.xbyvnqn@gmail.com>
2019-11-29 19:02:33 +0200

    markup for Phone component has been added

--- src/components/Phone/Phone.js
+++ src/components/Phone/Phone.js
@@ -8,7 +8,6 @@ const Phone = () => {
   const { phoneId } = useParams();
   const [phoneData, setPhoneData] = useState({});
 
-
   useEffect(() => {
     getPhoneById(phoneId)
       .then((data) => {
@@ -19,15 +18,97 @@ const Phone = () => {
       .catch((err) => console.error(err));
   }, []);
 
+  const {
+    name, description, battery, storage, connectivity,
+    android, sizeAndWeight, display, hardware, camera,
+  } = phoneData;
+
   return (
-    <>
-      <div>
-        Phone:
-        {`${phoneData.name}`}
-        {phoneId}
-      </div>
+    <article>
+      <h1>{name}</h1>
+      <p>{description}</p>
+      {!!phoneData.name && (
+      <ul className="specs">
+        <li className="specs__item">
+          <span>Battery</span>
+          <dl>
+            <dt>Type</dt>
+            <dd>{battery.type}</dd>
+            <dt>Standby time (max)</dt>
+            <dd>{battery.standbyTime}</dd>
+            <dt>Talk Time</dt>
+            <dd>{battery.talkTime}</dd>
+          </dl>
+        </li>
+        <li className="specs__item">
+          <span>Storage and Memory</span>
+          <dl>
+            <dt>Internal Storage</dt>
+            <dd>{storage.flash}</dd>
+            <dt>RAM</dt>
+            <dd>{storage.ram}</dd>
+          </dl>
+        </li>
+        <li className="specs__item">
+          <span>Connectivity</span>
+          <dl>
+            <dt>Network Support</dt>
+            <dd>{connectivity.cell}</dd>
+            <dt>WiFi</dt>
+            <dd>{connectivity.wifi}</dd>
+            <dt>Bluetooth</dt>
+            <dd>{connectivity.bluetooth}</dd>
+          </dl>
+        </li>
+        <li className="specs__item">
+          <span>Android</span>
+          <dl>
+            <dt>OS Version</dt>
+            <dd>{android.os}</dd>
+            <dt>UI</dt>
+            <dd>{android.ui}</dd>
+          </dl>
+        </li>
+        <li className="specs__item">
+          <span>Size and Weight</span>
+          <dl>
+            <dt>Dimensions</dt>
+            <dd>{sizeAndWeight.dimensions[0]}</dd>
+            <dd>{sizeAndWeight.dimensions[1]}</dd>
+            <dd>{sizeAndWeight.dimensions[2]}</dd>
+            <dt>Weight</dt>
+            <dd>{sizeAndWeight.weight}</dd>
+          </dl>
+        </li>
+        <li className="specs__item">
+          <span>Display</span>
+          <dl>
+            <dt>Screen size</dt>
+            <dd>{display.screenSize}</dd>
+            <dt>Screen resolution</dt>
+            <dd>{display.screenResolution}</dd>
+          </dl>
+        </li>
+        <li className="specs__item">
+          <span>Hardware</span>
+          <dl>
+            <dt>CPU</dt>
+            <dd>{hardware.cpu}</dd>
+            <dt>USB</dt>
+            <dd>{hardware.usb}</dd>
+          </dl>
+        </li>
+        <li className="specs__item">
+          <span>Camera</span>
+          <dl>
+            <dt>Primary</dt>
+            <dd>{camera.primary}</dd>
+          </dl>
+        </li>
+      </ul>
+      )}
       <Link to="/">Catalog</Link>
-    </>
+    </article>
   );
 };
 

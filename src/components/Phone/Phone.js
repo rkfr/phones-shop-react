3d3cf37a0e889fef8b26c05eb8b965bef0da4f54
rkfr <e.xbyvnqn@gmail.com>
2019-12-03 17:20:45 +0200

    added styles for Phone

--- src/components/Phone/Phone.js
+++ src/components/Phone/Phone.js
@@ -33,18 +33,18 @@ const Phone = () => {
   );
 
   return (
-    <article>
+    <article className="phone">
       <Link to="/">Catalog</Link>
       {!!phoneData.name && (
       <>
         <div className="previev">
           <div className="gallery previev__item">
-            <figure className="gallery__main">
-              <img src={selectedImageSrc} alt={name} className="gallery__main-image" />
-            </figure>
             <ul className="gallery__list">
               {images.map(renderGalleryItem)}
             </ul>
+            <figure className="gallery__main">
+              <img src={selectedImageSrc} alt={name} className="gallery__main-image" />
+            </figure>
           </div>
           <div className="description previev__item">
             <h1>{name}</h1>
@@ -53,79 +53,79 @@ const Phone = () => {
         </div>
         <ul className="specs">
           <li className="specs__item">
-            <span>Battery</span>
+            <h4 className="specs__title">Battery</h4>
             <dl>
-              <dt>Type</dt>
-              <dd>{battery.type}</dd>
-              <dt>Standby time (max)</dt>
-              <dd>{battery.standbyTime}</dd>
-              <dt>Talk Time</dt>
-              <dd>{battery.talkTime}</dd>
+              <dt className="specs__subtitle">Type</dt>
+              <dd className="specs__info">{battery.type}</dd>
+              <dt className="specs__subtitle">Standby time (max)</dt>
+              <dd className="specs__info">{battery.standbyTime}</dd>
+              <dt className="specs__subtitle">Talk Time</dt>
+              <dd className="specs__info">{battery.talkTime}</dd>
             </dl>
           </li>
           <li className="specs__item">
-            <span>Storage and Memory</span>
+            <h4 className="specs__title">Storage and Memory</h4>
             <dl>
-              <dt>Internal Storage</dt>
-              <dd>{storage.flash}</dd>
-              <dt>RAM</dt>
-              <dd>{storage.ram}</dd>
+              <dt className="specs__subtitle">Internal Storage</dt>
+              <dd className="specs__info">{storage.flash}</dd>
+              <dt className="specs__subtitle">RAM</dt>
+              <dd className="specs__info">{storage.ram}</dd>
             </dl>
           </li>
           <li className="specs__item">
-            <span>Connectivity</span>
+            <h4 className="specs__title">Connectivity</h4>
             <dl>
-              <dt>Network Support</dt>
-              <dd>{connectivity.cell}</dd>
-              <dt>WiFi</dt>
-              <dd>{connectivity.wifi}</dd>
-              <dt>Bluetooth</dt>
-              <dd>{connectivity.bluetooth}</dd>
+              <dt className="specs__subtitle">Network Support</dt>
+              <dd className="specs__info">{connectivity.cell}</dd>
+              <dt className="specs__subtitle">WiFi</dt>
+              <dd className="specs__info">{connectivity.wifi}</dd>
+              <dt className="specs__subtitle">Bluetooth</dt>
+              <dd className="specs__info">{connectivity.bluetooth}</dd>
             </dl>
           </li>
           <li className="specs__item">
-            <span>Android</span>
+            <h4 className="specs__title">Android</h4>
             <dl>
-              <dt>OS Version</dt>
-              <dd>{android.os}</dd>
-              <dt>UI</dt>
-              <dd>{android.ui}</dd>
+              <dt className="specs__subtitle">OS Version</dt>
+              <dd className="specs__info">{android.os}</dd>
+              <dt className="specs__subtitle">UI</dt>
+              <dd className="specs__info">{android.ui}</dd>
             </dl>
           </li>
           <li className="specs__item">
-            <span>Size and Weight</span>
+            <h4 className="specs__title">Size and Weight</h4>
             <dl>
-              <dt>Dimensions</dt>
-              <dd>{sizeAndWeight.dimensions[0]}</dd>
-              <dd>{sizeAndWeight.dimensions[1]}</dd>
-              <dd>{sizeAndWeight.dimensions[2]}</dd>
-              <dt>Weight</dt>
-              <dd>{sizeAndWeight.weight}</dd>
+              <dt className="specs__subtitle">Dimensions</dt>
+              <dd className="specs__info">{sizeAndWeight.dimensions[0]}</dd>
+              <dd className="specs__info">{sizeAndWeight.dimensions[1]}</dd>
+              <dd className="specs__info">{sizeAndWeight.dimensions[2]}</dd>
+              <dt className="specs__subtitle">Weight</dt>
+              <dd className="specs__info">{sizeAndWeight.weight}</dd>
             </dl>
           </li>
           <li className="specs__item">
-            <span>Display</span>
+            <h4 className="specs__title">Display</h4>
             <dl>
-              <dt>Screen size</dt>
-              <dd>{display.screenSize}</dd>
-              <dt>Screen resolution</dt>
-              <dd>{display.screenResolution}</dd>
+              <dt className="specs__subtitle">Screen size</dt>
+              <dd className="specs__info">{display.screenSize}</dd>
+              <dt className="specs__subtitle">Screen resolution</dt>
+              <dd className="specs__info">{display.screenResolution}</dd>
             </dl>
           </li>
           <li className="specs__item">
-            <span>Hardware</span>
+            <h4 className="specs__title">Hardware</h4>
             <dl>
-              <dt>CPU</dt>
-              <dd>{hardware.cpu}</dd>
-              <dt>USB</dt>
-              <dd>{hardware.usb}</dd>
+              <dt className="specs__subtitle">CPU</dt>
+              <dd className="specs__info">{hardware.cpu}</dd>
+              <dt className="specs__subtitle">USB</dt>
+              <dd className="specs__info">{hardware.usb}</dd>
             </dl>
           </li>
           <li className="specs__item">
-            <span>Camera</span>
+            <h4 className="specs__title">Camera</h4>
             <dl>
-              <dt>Primary</dt>
-              <dd>{camera.primary}</dd>
+              <dt className="specs__subtitle">Primary</dt>
+              <dd className="specs__info">{camera.primary}</dd>
             </dl>
           </li>
         </ul>
